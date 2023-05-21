4ff6488719de7e7f39f4c7f82d4828a128645b6c
Syed Jahangeer Junaid <34482648+jahangeer010@users.noreply.github.com>
2023-05-21 17:28:25 +0500

    2nd update

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/components/HomeScreen';
+import HomeScreen from './components/Screens/HomeScreen';
+import MapsScreen from './components/Screens/MapsScreen';
 import { Provider } from 'react-redux'
 import { Store } from './redux/Store'
 
@@ -13,13 +14,21 @@ const Stack = createNativeStackNavigator();
     <Provider store={Store}>  
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          headerShown:false
+        }}/>
+         <Stack.Screen name="MapsScreen" component={MapsScreen} options={{
+          headerShown:false
+        }}/>
       </Stack.Navigator>
     </NavigationContainer>
     </Provider>
-  
+
   )
 }
+//for google maps goto https://github.com/FaridSafi/react-native-google-places-autocomplete
+//npm install react-native-maps
+//npm install react-native-maps-directions
 // Commands to Install for navigation
 //npm install react-native-screens react-native-safe-area-context
 //npm install @react-navigation/native
@@ -32,5 +41,8 @@ const Stack = createNativeStackNavigator();
 //npm install react-native-vector-icons
 //paste the following line in app src gradle file
 //apply from: "../../node_modules/react-native-vector-icons/fonts.gradle"
-
+//FOR ENV FILES 
+//npm i react-native-dotenv
+//after install of dotenv goto file babelconfig
+// if you want fresh install all Delete Node modules by command  rm -rf node_modules
 export default App
